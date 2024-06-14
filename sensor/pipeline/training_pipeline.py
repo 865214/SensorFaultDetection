0ef2b8385b819953c46d97f9774e0cccf244690d
Zohebkazi <kazizoheb59@gmail.com>
2024-06-14 22:47:45 +0530

    Data Transformation Completed

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,4 +1,4 @@
-from sensor.entity.config_entity import DataIngestionConfig, DataValidationConfig, TrainingPipelineConfig
+from sensor.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, TrainingPipelineConfig
 from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 
 from sensor.exception import CustomException
@@ -6,6 +6,7 @@ import sys, os
 from sensor.logger import logging
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
+from sensor.components.data_transformation import DataTransformation
 from datetime import datetime
 
 class TrainPipeline:
@@ -37,10 +38,18 @@ class TrainPipeline:
         except Exception as e:
             raise CustomException(e, sys)
     
+    def start_data_transformation(self, data_validation_artifact: DataValidationArtifact):
+        try:
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact, data_transformation_config=data_transformation_config)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except Exception as e:
+            raise CustomException(e, sys)    
     def run_pipeline(self):
         try:
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact= data_ingestion_artifact)
-            
+            data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
         except Exception as e:
             raise CustomException(e, sys)
